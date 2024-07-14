d434217c243209be5b117ea8eb6e69b5fdc50d42
ALEM  FREDY <alemfredy@yahoo.com>
2024-07-14 17:33:38 -0400

    backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "primuslearning-app"
+    bucket = "primuslearning-app500"
+     key = "terraform.tfstate"
     region = "us-east-1"
-    key = "eks/terraform.tfstate"
   }
 }
